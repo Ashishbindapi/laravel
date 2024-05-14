4aca0ce90fcf5550c9584c3ecdef3312aa0feae4
ashishbindraji <167048373+ashishbindraji@users.noreply.github.com>
2024-05-14 13:48:12 +0530

    laravel_5

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group">
+    <label for="">{{$lable}}</label>
+    <input type="{{$type}}" name="{{$name}}" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{old('name')}}">
+    <span class="text-danger">
+        {{$demo}}
+        @error('name')
+            {{$message}}
+        @enderror
+    </span>
+</div>
