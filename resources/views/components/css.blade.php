b4cc48239b29d3d1131c493f99185946e3162ab1
ashishbindraji <167048373+ashishbindraji@users.noreply.github.com>
2024-05-14 13:48:12 +0530

    laravel_5

---
+++ resources/views/components/css.blade.php
@@ -0,0 +1,19 @@
+<style>
+.nav-link {
+    color: #050000 !important;
+    font-size: 18px;
+    font-weight: bold;
+    transition: background-color 0.3s, color 0.3s;
+    border: 1px solid black
+}
+.nav-link:hover {
+    background-color: #495057;
+    border-radius: 5px;
+    color: #f8f9fa !important;
+}
+.navbar {
+    background-color: #343a40;
+    border-radius: 5px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
