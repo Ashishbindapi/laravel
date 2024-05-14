02473971a0bb0b9aad74f87600d7b5785235b41c
ashishbindraji <167048373+ashishbindraji@users.noreply.github.com>
2024-05-14 13:48:12 +0530

    laravel_5

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        return view('customer');
+    }
+
+    public Function store(Request $request)
+    {
+        $customer = new Customer;
+
+        $customer->name = $request['name'];
+        $customer->email = $request['email'];
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->country = $request['country'];
+        $customer->state = $request['state'];
+        $customer->dob = $request['dob'];
+        $customer->password = md5($request['password']);
+        $customer->save();
+
+        return redirect('/customer/view');
+    }
+
+    public function view()
+    {
+        $customers = Customer::all();
+        $data = compact('customers');
+        return view('customer-view')->with($data);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\CustomerController;
+
+Route::get('/register', [RegistrationController::class, 'index']);
+Route::post('/register', [RegistrationController::class, 'register']);
+Route::get('/customer', [CustomerController::class, 'index']);
+Route::post('/customer', [CustomerController::class, 'store']);
+Route::get('/customer/view', [CustomerController::class, 'view']);
